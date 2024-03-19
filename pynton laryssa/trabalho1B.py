92ad1ef944613cf7a48c5a8b69d5aaad5fa3cf4b
laryssa <ferreiralaryssa@gmail.com>
2024-03-18 21:48:30 -0300

    trabalho1b

---
+++ pynton laryssa/trabalho1B.py
@@ -0,0 +1,8 @@
+
+entrada1=int(input("Entre com nota 1:"))
+raiz=(entrada1**(1/3))
+print(raiz)
+if raiz >= 10:
+    print("nao é necessario o uso de calculadora!")
+else:
+    print("é necessario o uso de calculadora!")    
